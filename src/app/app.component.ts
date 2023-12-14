c84caf2403da46f7d65b8a10f78f62a6ac903448
Éleson Silva <elesonsilva9@gmail.com>
2023-12-14 11:20:27 -0300

    upadates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./components/cabecalho/cabecalho.component";
+import { CorpoComponent } from "./components/corpo/corpo.component";
+import { RodapeComponent } from "./components/rodape/rodape.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, CabecalhoComponent, CorpoComponent, RodapeComponent]
 })
 export class AppComponent {
   title = 'ZonaAzul';
