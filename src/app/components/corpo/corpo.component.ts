e20e810b978b338d8b674ad77df17a223affb83e
Éleson Silva <elesonsilva9@gmail.com>
2023-12-15 11:53:31 -0300

    UPDATES

--- src/app/components/corpo/corpo.component.ts
+++ src/app/components/corpo/corpo.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-corpo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './corpo.component.html',
   styleUrl: './corpo.component.css'
 })
 export class CorpoComponent {
 
+ back ='';
+ 
+
+
+
+  abrirCopacabana():void{
+      this.back = "back"
+  }
 }
